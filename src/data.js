ea9248e7e2ae40174fd4d313ff87a498bebf5767
moniyama <monicasayama@gmail.com>
2019-09-12 14:50:04 -0300

    redução dos cálculos para 1 linha

--- src/data.js
+++ src/data.js
@@ -1,7 +1,5 @@
 const calculadora = (velocidade, ovo, incubadora, tempoDiario) => { //incubadora = 1 ou super = 0,667
-  let distancia = ovo*incubadora;  // distancia em Km
-  let t = distancia/velocidade; // tempo t em hora
-  let dias = ~~(t/(tempoDiario/60));  // transforma tempo em hora
+  let dias = ~~((ovo*incubadora/velocidade)/(tempoDiario/60));
   return dias
 };
 
