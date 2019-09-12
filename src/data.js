7571fe75bbb718ccd994cb83d3a9f4d37d25f3a0
moniyama <monicasayama@gmail.com>
2019-09-12 14:44:47 -0300

    os dias são números inteiros

--- src/data.js
+++ src/data.js
@@ -1,10 +1,8 @@
-const calculadora = (velocidade, ovo, incubadora, tempo) => { //incubadora = 1 ou super = 0,664
+const calculadora = (velocidade, ovo, incubadora, tempoDiario) => { //incubadora = 1 ou super = 0,667
   let distancia = ovo*incubadora;  // distancia em Km
   let t = distancia/velocidade; // tempo t em hora
-  let dias = t/(tempo/60);  // transforma tempo em hora
+  let dias = ~~(t/(tempoDiario/60));  // transforma tempo em hora
   return dias
-
-  console.log(dias);
 };
 
 window.data = calculadora;
