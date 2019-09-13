3ebf7aa798d4be27d4b56bd039ae983fab023146
moniyama <monicasayama@gmail.com>
2019-09-13 12:55:31 -0300

    resultando sendo printado na tela

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const result = document.getElementsByClassName('resultado');
+const velocidade = 5;
+const ovo = 10;
+const incubadora = 1;
+const tempoDiario = 15;
+
+result[0].innerHTML = `Aproximadamente ${calculadora(velocidade, ovo, incubadora, tempoDiario)} dias `
