23c466d8c0be2fb2ace479854d5fe2f49865ad4a
rosi-souza <rosi.souza@live.com>
2019-09-16 16:46:39 -0300

    minor change

--- src/main.js
+++ src/main.js
@@ -5,3 +5,13 @@ const incubadora = 1;
 const tempoDiario = 15;
 
 result[0].innerHTML = `Aproximadamente ${calculadora(velocidade, ovo, incubadora, tempoDiario)} dias `
+
+const listapokemon = POKEMON.pokemon;
+
+let printarpoke = document.getElementById("pokemon");
+
+const pokemonmap = listapokemon.filter(x => x.egg === "2 km");
+const banana = pokemonmap.map(y => y.name + y.egg + `<img src="${y.img}"/>`);
+printarpoke.innerHTML = banana;
+
+console.log(pokemonmap);
