1b142c0504a66cded49d322f0a7f2e097f60c811
rosi-souza <rosi.souza@live.com>
2019-09-16 01:45:58 -0300

    filter pokemon

--- src/data.js
+++ src/data.js
@@ -1,8 +1,18 @@
 // esta é uma função de exemplo
 // veja como agregamos a função ao objeto global window
+// const POKEMONS = POKEMON.pokemon;
 
-const example = () => {
-  return "id";
-};
+// const example = () => {
+//   return "id";
+// };
 
-window.data = example;
+
+// let image = []
+
+// const filterNames = () => {
+//   for (let x of POKEMONS) {
+//     x.name + `<img src="${x.img}"/>`;
+     
+//   }  
+// };
+// window.data = filterNames;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
-const POKEMONS = POKEMON.pokemon;
+const listapokemon = POKEMON.pokemon;
 
-for(let x of POKEMONS){
-  document.write('<img src="'+x.img+'">')
-}
+let printarpoke = document.getElementById("pokemon");
 
+// for (let x of listapokemon) {
+//   let banana = x.name;
+//   // printarpoke.innerHTML += `<img src="${x.img}"/>` + x.name + x.egg + `<br>`;
+//   // console.log(banana)
+// }
+
+// const pokemonmap = listapokemon.map(x =>`<img src="${x.img}"/>` + x.name + x.egg);
+// pokemonmap.filter((x) => {
+//   return x.egg === "2 km";
+// }),
+// printarpoke.innerHTML = pokemonmap;
+
+// printarpoke.innerHTML += filterNames();
+
+const pokemonmap = listapokemon.filter(x => x.egg === "2 km");
+const banana = pokemonmap.map(y => y.name + y.egg + `<img src="${y.img}"/>`);
+printarpoke.innerHTML = banana;
+
+// for (let y of pokemonmap
+// ) {
+//   printarpoke.innerHTML = y;
+// }
+console.log(pokemonmap);
