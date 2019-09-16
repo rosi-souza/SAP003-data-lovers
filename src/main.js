b814e11cb7d8b573e8e4341d14db7f9637747e6e
rosi-souza <rosi.souza@live.com>
2019-09-16 13:50:01 -0300

    minor changes

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ const listapokemon = POKEMON.pokemon;
 
 let printarpoke = document.getElementById("pokemon");
 
+const pokemonmap = listapokemon.filter(x => x.egg === "2 km");
+const banana = pokemonmap.map(y => y.name + y.egg + `<img src="${y.img}"/>`);
+printarpoke.innerHTML = banana;
+
+console.log(pokemonmap);
+
+// for (let y of pokemonmap
+// ) {
+//   printarpoke.innerHTML = y;
+// }
+
 // for (let x of listapokemon) {
 //   let banana = x.name;
 //   // printarpoke.innerHTML += `<img src="${x.img}"/>` + x.name + x.egg + `<br>`;
@@ -16,12 +27,3 @@ let printarpoke = document.getElementById("pokemon");
 
 // printarpoke.innerHTML += filterNames();
 
-const pokemonmap = listapokemon.filter(x => x.egg === "2 km");
-const banana = pokemonmap.map(y => y.name + y.egg + `<img src="${y.img}"/>`);
-printarpoke.innerHTML = banana;
-
-// for (let y of pokemonmap
-// ) {
-//   printarpoke.innerHTML = y;
-// }
-console.log(pokemonmap);
