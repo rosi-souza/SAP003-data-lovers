9c5496e1522c12678d71910e3355ccdbf7d026cf
rosi-souza <rosi.souza@live.com>
2019-09-17 17:36:31 -0300

    add css

--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
 const calculadora = (velocidade, ovo, incubadora, tempoDiario) => { //incubadora = 1 ou super = 0,667
   let dias = ~~((ovo*incubadora/velocidade)/(tempoDiario/60));
-  return dias
+  return dias;
 };
 
 const filtro = (array, ovo) => {
-  return array.filter(x => x.egg == ovo)      
-}
+  return array.filter(x => x.egg == ovo);
+};
 
 window.data = calculadora;
 window.data = filtro;
--- src/main.js
+++ src/main.js
@@ -1,26 +1,30 @@
 // calculadora
-const result = document.getElementsByClassName('resultado');
+const result = document.getElementsByClassName("resultado");
 const velocidade = 5;
 const ovo = 10;
 const incubadora = 1;
 const tempoDiario = 15;
 
-result[0].innerHTML = `Aproximadamente ${calculadora(velocidade, ovo, incubadora, tempoDiario)} dias `
+result[0].innerHTML = `Aproximadamente ${calculadora(velocidade, ovo, incubadora, tempoDiario)} dias `;
 
 // filtro
 const printarpoke = document.getElementById("pokemon");
 const listapokemon = POKEMON.pokemon;
 
-radio = document.getElementById("radio")
+radio = document.getElementById("radio");
 
 // try3
 radio.addEventListener("change", filtrado = () => {
-    const filtrado = filtro(listapokemon, event.target.value)
-    printarpoke.innerHTML = ""
-    filtrado.forEach(element => {
-    printarpoke.innerHTML += `${element.name} <img src = "${element.img}">`;
-    });
-})
+  const filtrado = filtro(listapokemon, event.target.value);
+  printarpoke.innerHTML = "";
+  filtrado.forEach(element => {
+printarpoke.innerHTML += `<div class="card"><div class="card-pokemon">
+			${element.name}
+			<img class="image" src = "${element.img}">
+		</div>
+	</div>`;
+  });
+});
 
 //try2
 // const filtrado = filtro(listapokemon, 2)
