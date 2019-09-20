cfdc0c62e26b2683bc8b59cb01f14c8273a011f5
rosi-souza <rosi.souza@live.com>
2019-09-20 15:20:43 -0300

    Squashed commit of the following:
    commit 90f039553e063b9111a79905200b7ab49a3fe687
    Merge: afa4a37 5208d20
    Author: Monica Yamazaki <52083679+moniyama@users.noreply.github.com>
    Date:   Fri Sep 20 15:10:38 2019 -0300
    
        Merge pull request #7 from moniyama/mon-sort
    
        Mon sort
    
    commit 5208d204f45972c6c29ed99307464b4441dd3f19
    Author: moniyama <monicasayama@gmail.com>
    Date:   Thu Sep 19 18:24:24 2019 -0300
    
        ordenação separada
    
    commit ca423756fb89cee33cec7734e0cacc5d1ae18cc2
    Author: moniyama <monicasayama@gmail.com>
    Date:   Wed Sep 18 14:41:02 2019 -0300
    
        adicionado a função de ordenação no data.js
    
    commit 99d16c1e60fb2fd9606343b7a77866c7bbbb56d7
    Author: moniyama <monicasayama@gmail.com>
    Date:   Tue Sep 17 17:59:50 2019 -0300
    
        adicionando o style.css que sumiu T_T
    
    commit 590f300d6c7becc7d444aa2e0fe19e2da3ac868e
    Author: moniyama <monicasayama@gmail.com>
    Date:   Tue Sep 17 17:57:52 2019 -0300
    
        inicio
    
    commit c75369eee39ecbf56d4e6fd3ff939adfa0e5a177
    Author: moniyama <monicasayama@gmail.com>
    Date:   Tue Sep 17 16:07:47 2019 -0300
    
        adicionado a função de ordenação por nome no main

--- src/data.js
+++ src/data.js
@@ -7,6 +7,17 @@ const filtro = (array, ovo) => {
   return array.filter(x => x.egg == ovo);
 };
 
+const ordem = (array, data) => {
+  array.sort((a,b) => {
+    if (a[data] < b[data])
+      return -1;    
+    if (a[data] > b[data])
+      return 1; 
+    return 0;
+  })
+  return array
+}
+
 window.data = calculadora;
 window.data = filtro;
-
+window.data = ordem;
--- src/main.js
+++ src/main.js
@@ -7,29 +7,30 @@ const tempoDiario = 15;
 
 result[0].innerHTML = `Aproximadamente ${calculadora(velocidade, ovo, incubadora, tempoDiario)} dias `;
 
-// filtro
+// filtro e ordenação
 const printarpoke = document.getElementById("pokemon");
 const listapokemon = POKEMON.pokemon;
 
-radio = document.getElementById("radio");
+radio = document.getElementById("radio")
+radio.addEventListener("change", () => {
+    filtrado = filtro(listapokemon, event.target.value);
+    printar(filtrado)
+})
+select = document.getElementById("select")
+select.addEventListener("change", () => {
+    ordenado = ordem(filtrado, event.target.value)
+    printar(ordenado)
+    });
 
-// try3
-radio.addEventListener("change", filtrado = () => {
-  const filtrado = filtro(listapokemon, event.target.value);
-  printarpoke.innerHTML = "";
-  filtrado.forEach(element => {
-    printarpoke.innerHTML += `
-  <div class="card">
-    <div class="card-pokemon">
-      <img class="image" src = "${element.img}">
-      <span class="name">${element.name}</span>
-		</div>
-	</div>`;
-  });
-});
-
-//try2
-// const filtrado = filtro(listapokemon, 2)
-// filtrado.forEach(element => {
-// printarpoke.innerHTML += `${element.name} <img src = "${element.img}">`;
-// })
+const printar = array => {
+    let layout = "";
+    printarpoke.innerHTML = ""
+    array.forEach(element => {layout += `
+    <div class="card">
+      <div class="card-pokemon">
+        <img src = "${element.img}">
+        <span class="name"> ${element.id}.${element.name} Tipo:${element.type}</span>
+      </div>
+    </div>`})
+    printarpoke.innerHTML = layout;
+}
